379838c050fad8a6d8f2d18e7df4c9d1ba240ccc
Matthew Crandall <mcranda1@byu.edu>
2024-11-02 10:40:37 -0600

    commit test

--- src/app.jsx
+++ src/app.jsx
@@ -1,45 +1,60 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Main } from './main/main';
+import { Builder } from './builder/builder';
 
 export default function App() {
   return (
-    <div className='body bg-dark text-light'>
-      <header className='container-fluid'>
-        <nav className='navbar fixed-top navbar-dark'>
-          <div className='navbar-brand'>
-            Simon<sup>&reg;</sup>
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              Pokemon Team Builder<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='Login'>
+                  Login
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='Main'>
+                  Main
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='Builder'>
+                  Builder
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/main' element={<Main />} />
+          <Route path='/builder' element={<Builder />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Author Name(s)</span>
+            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+              Source
+            </a>
           </div>
-          <menu className='navbar-nav'>
-            <li className='nav-item'>
-              <a className='nav-link' href='index.html'>
-                Home
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='play.html'>
-                Play
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='scores.html'>
-                Scores
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
-
-      <main>App components go here</main>
-
-      <footer className='bg-dark text-white-50'>
-        <div className='container-fluid'>
-          <span className='text-reset'>Author Name(s)</span>
-          <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
-            Source
-          </a>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
