c1c822384ae850642ce4911e44971cdbf7b21521
Matthew Crandall <mcranda1@byu.edu>
2024-11-02 10:35:46 -0600

    begin building jsx

---
+++ src/builder/builder.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Builder() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>builder displayed here</div>
+    </main>
+  );
+}
