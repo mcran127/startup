72aa6acd67d50105041c9373228fa57df26c7e70
Matthew Crandall <mcranda1@byu.edu>
2024-11-04 14:44:12 -0700

    editing footer

--- src/app.jsx
+++ src/app.jsx
@@ -42,7 +42,7 @@ export default function App() {
         <footer className='bg-dark text-white-50'>
           <div className='container-fluid'>
             <span className='text-reset'>Author Name(s)</span>
-            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+            <a className='text-reset' href='https://github.com/mcran127/startup'>
               Source
             </a>
           </div>
