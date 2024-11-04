2fb5ad7a9c6f0edb27853cbf7c0d06703c3bcc0a
Matthew Crandall <mcranda1@byu.edu>
2024-11-04 13:10:17 -0700

    added correct taggin to main

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -4,7 +4,42 @@ import './main.css';
 export function Main() {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>main displayed here</div>
+      <h1>
+          <span>
+              <div>Latest Decks</div>
+              <a href="builder.html" id="newDeck">
+                  <button type="button">New Deck</button>
+              </a>
+          </span>
+      </h1>
+      <section>
+          <table>
+              <tr id="names">
+                <th>TITLE by NAME</th>
+                <th>TITLE by NAME</th>
+                <th>TITLE by NAME</th>
+                <th>TITLE by NAME</th>
+              </tr>
+              <tr id = "images">
+                <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+                <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+                <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+                <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+              </tr>
+              <tr id="names">
+                  <th>TITLE by NAME</th>
+                  <th>TITLE by NAME</th>
+                  <th>TITLE by NAME</th>
+                  <th>TITLE by NAME</th>
+                </tr>
+                <tr id = "images">
+                  <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+                  <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+                  <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+                  <td><img src="https://static.wikia.nocookie.net/257d534d-755d-4547-be64-dabb04fac8b9/scale-to-width/755" alt="" height={200} width={300}></img></td>
+                </tr>
+            </table>
+      </section>
     </main>
   );
 }
