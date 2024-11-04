655c1ea7659f50754fe803c4248fded3ee14b200
Matthew Crandall <mcranda1@byu.edu>
2024-11-04 15:11:01 -0700

    adjusting where buttons lead

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,7 +5,7 @@ export function Login() {
     <main className='container-fluid bg-secondary text-center'>
         <h1>Welcome to Pokemon Team Builder</h1>
         <h2>Login Here</h2>
-        <form method="get" action="main.html">
+        <form method="get" action="main">
             <div>
                 <span>Username</span>
                 <input type="text" placeholder="John Smith"></input>
