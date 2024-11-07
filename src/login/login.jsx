8ad76ab2019561e1e107f76ee519beac566048a0
Matthew Crandall <mcranda1@byu.edu>
2024-11-07 12:37:42 -0700

    login test

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { AuthState } from './authState';
 
-export function Login() {
+export function Login(props) {
 
     const [userName, setUserName] = React.useState(props.userName);
     const [password, setPassword] = React.useState('');
 
     async function createUser() {
         localStorage.setItem('userName', userName);
-        props.onLogin(userName);
+        props.onAuthChange(userName, AuthState.Authenticated);
       }
 
   return (
     <main className='container-fluid bg-secondary text-center'>
         <h1>Welcome to Pokemon Team Builder</h1>
         <h2>Login Here</h2>
-        <form method="get" action="main">
+        <form method="get" action="main" onSubmit={createUser}>
             <div>
                 <span>Username</span>
                 <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="John Smith"></input>
@@ -24,7 +25,7 @@ export function Login() {
                 <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password"></input>
             </div>
             <div>
-                <button type="submit" onClick={() => createUser()} disabled={!userName || !password}>Login</button>
+                <button type="submit" disabled={!userName || !password}>Login</button>
             </div>
         </form>
     </main>
