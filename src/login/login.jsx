d995fc0084bca7ad0b6f340e71d85010788cb035
Matthew Crandall <mcranda1@byu.edu>
2024-11-07 12:32:47 -0700

    working on login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 export function Login() {
 
+    const [userName, setUserName] = React.useState(props.userName);
+    const [password, setPassword] = React.useState('');
+
     async function createUser() {
         localStorage.setItem('userName', userName);
         props.onLogin(userName);
       }
-      
+
   return (
     <main className='container-fluid bg-secondary text-center'>
         <h1>Welcome to Pokemon Team Builder</h1>
@@ -14,11 +17,11 @@ export function Login() {
         <form method="get" action="main">
             <div>
                 <span>Username</span>
-                <input type="email" placeholder="John Smith"></input>
+                <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="John Smith"></input>
             </div>
             <div>
                 <span>Password</span>
-                <input type="password" placeholder="password"></input>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password"></input>
             </div>
             <div>
                 <button type="submit" onClick={() => createUser()} disabled={!userName || !password}>Login</button>
