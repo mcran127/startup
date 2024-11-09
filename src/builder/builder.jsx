c416e2b54213e641cab99362213235457d957689
Matthew Crandall <mcranda1@byu.edu>
2024-11-09 09:18:33 -0700

    working on builder

--- src/builder/builder.jsx
+++ src/builder/builder.jsx
@@ -1,43 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './builder.css';
 
 export function Builder() {
+
+    const [pokemonList] = useState([
+        { name: 'Bulbasaur', image: 'https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/500px-0001Bulbasaur.png', stats: { hp: 45, attack: 49, defense: 49, specialAttack: 65, specialDefense: 65, speed: 45 } },
+        { name: 'Choose your Pokemon', image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png", stats: { hp: 0, attack: 0, defense: 0, specialAttack: 0, specialDefense: 0, speed: 0} },
+        { name: 'Choose your Pokemon', image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png", stats: { hp: 0, attack: 0, defense: 0, specialAttack: 0, specialDefense: 0, speed: 0} },
+        { name: 'Choose your Pokemon', image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png", stats: { hp: 0, attack: 0, defense: 0, specialAttack: 0, specialDefense: 0, speed: 0} },
+        { name: 'Choose your Pokemon', image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png", stats: { hp: 0, attack: 0, defense: 0, specialAttack: 0, specialDefense: 0, speed: 0} },
+        { name: 'Choose your Pokemon', image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png", stats: { hp: 0, attack: 0, defense: 0, specialAttack: 0, specialDefense: 0, speed: 0} },
+      ]);
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedPokemon, setSelectedPokemon] = useState(pokemonList[0]);
+
+    const filteredPokemon = pokemonList.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    
+    const handlePokemonSelect = (pokemon) => {
+        setSelectedPokemon(pokemon);
+    };
+
   return (
     <main className='container-fluid bg-secondary text-center'>
       <h1>Choose Your Pokemon</h1>
         <table>
+            <tbody>
             <tr>
-                <td><img src="https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/500px-0001Bulbasaur.png" alt=""></img></td>
-                <td><img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png" alt=""></img></td>
-                <td><img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png" alt=""></img></td>
-                <td><img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png" alt=""></img></td>
-                <td><img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png" alt=""></img></td>
-                <td><img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Black_question_mark.png" alt=""></img></td>
+                {filteredPokemon.map((pokemon, index) => (
+                <td key={index} onClick={() => handlePokemonSelect(pokemon)}>
+                    <img src={pokemon.image} alt={pokemon.name} />
+                </td>
+                ))}
             </tr>
             <tr>
-                <td>Pokemon Name Here</td>
-                <td>Pokemon Name Here</td>
-                <td>Pokemon Name Here</td>
-                <td>Pokemon Name Here</td>
-                <td>Pokemon Name Here</td>
-                <td>Pokemon Name Here</td>
+                {filteredPokemon.map((pokemon, index) => (
+                <td key={index}>{pokemon.name}</td>
+                ))}
             </tr>
+            </tbody>
         </table>
+        
         <div className="search">
             <form action="#">
                 <input type="text" placeholder="Search Pokemon"
-                    name="search">
+                    name="search" value = {searchTerm} onChange={handleSearchChange}>
                 </input>
                 <button>
                 <i class="fa fa-search">Search</i>
                 </button>
             </form>
         </div>
+
         <div>
             <h2>Stats of Pokemon</h2>
             <div id = "combined">
                 <div id = "currPokemon">
-                    <img src="https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/500px-0001Bulbasaur.png" alt=""></img>
+                    <img src={selectedPokemon.image} alt={selectedPokemon.name}></img>
                 </div>
                 <table id="stats">
                     <tr>
@@ -45,24 +70,24 @@ export function Builder() {
                         <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbJ1e0DJuvhm69F3mOg4NpsS4Y4x4A0QI-Hg&s" alt=""></img></td>
                     </tr>
                     <tr>
-                        <td>100</td>
-                        <td>100</td>
+                        <td>{selectedPokemon.stats.hp}</td>
+                        <td>{selectedPokemon.stats.attack}</td>
                     </tr>
                     <tr>
                         <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbJ1e0DJuvhm69F3mOg4NpsS4Y4x4A0QI-Hg&s" alt=""></img></td>
                         <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbJ1e0DJuvhm69F3mOg4NpsS4Y4x4A0QI-Hg&s" alt=""></img></td>
                     </tr>
                     <tr>
-                        <td>100</td>
-                        <td>100</td>
+                        <td>{selectedPokemon.stats.defense}</td>
+                        <td>{selectedPokemon.stats.specialAttack}</td>
                     </tr>
                     <tr>
                         <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbJ1e0DJuvhm69F3mOg4NpsS4Y4x4A0QI-Hg&s" alt=""></img></td>
                         <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbJ1e0DJuvhm69F3mOg4NpsS4Y4x4A0QI-Hg&s" alt=""></img></td>
                     </tr>
                     <tr>
-                        <td>100</td>
-                        <td>100</td>
+                        <td>{selectedPokemon.stats.specialDefense}</td>
+                        <td>{selectedPokemon.stats.speed}</td>
                     </tr>
                 </table>
             </div>
