fa477915169804f2369b04f5ce58e164bd6f7dc4
Matthew Crandall <mcranda1@byu.edu>
2024-11-14 15:17:48 -0700

    stupid errors

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -12,7 +12,7 @@ export function Login(props) {
 
     async function createUser(event) {
         event.preventDefault();
-        console.log('Creating user:', userName, password); 
+        console.log('Creating user:', userName, password);
       
         const response = await fetch(`/api/auth/create`, {
           method: 'POST',
