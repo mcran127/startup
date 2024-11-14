6c04cca5a10686d6cf3108286ff87517afece78c
Matthew Crandall <mcranda1@byu.edu>
2024-11-14 15:34:22 -0700

    not sure

--- service/index.js
+++ service/index.js
@@ -14,6 +14,10 @@ app.use(`/api`, apiRouter);
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
