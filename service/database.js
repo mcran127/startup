2a1483a45446d97ec9fde01aebd076cded1e5304
Matthew Crandall <mcranda1@byu.edu>
2024-11-22 15:28:18 -0700

    forgot to save my user collelction test before retesting and got the same error like an idiot :)

--- service/database.js
+++ service/database.js
@@ -7,6 +7,8 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('startup');
 
+const userCollection = db.collection('user');
+
 (async function testConnection() {
     await client.connect();
     await db.command({ ping: 1 });
